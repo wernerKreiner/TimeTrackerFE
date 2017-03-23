a58411cf75d03e488c3e0e7926a2537a8aa6e59d
gruber <gruber.se@gmail.com>
2017-03-23 16:20:24 +0100

    test

--- app/src/main/java/at/jku/se/timetrackerfrontend/HomeScreenActivity.java
+++ app/src/main/java/at/jku/se/timetrackerfrontend/HomeScreenActivity.java
@@ -9,5 +9,6 @@ public class HomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        System.out.println("hallo ");
     }
 }
