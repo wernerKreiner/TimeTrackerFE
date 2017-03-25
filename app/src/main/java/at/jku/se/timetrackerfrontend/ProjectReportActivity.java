06541e80c00e2f71dd80ce5e3194a02b3e8be720
gruber <gruber.se@gmail.com>
2017-03-25 09:53:58 +0100

    project report

--- app/src/main/java/at/jku/se/timetrackerfrontend/ProjectReportActivity.java
+++ app/src/main/java/at/jku/se/timetrackerfrontend/ProjectReportActivity.java
@@ -2,6 +2,8 @@ package at.jku.se.timetrackerfrontend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ProjectReportActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ProjectReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_report);
+
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner_projectReport_projectSelection);
+        String[] items = new String[]{"1", "2", "three"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
 }
