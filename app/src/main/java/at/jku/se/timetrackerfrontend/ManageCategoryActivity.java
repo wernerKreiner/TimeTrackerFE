dfba0eb9d0db1cc1342b62d68459c6ca8b7370dc
LampD <dominik.lamprecht@icloud.com>
2017-03-26 17:15:50 +0200

    manage category (listview finished)

--- app/src/main/java/at/jku/se/timetrackerfrontend/ManageCategoryActivity.java
+++ app/src/main/java/at/jku/se/timetrackerfrontend/ManageCategoryActivity.java
@@ -1,25 +1,32 @@
 package at.jku.se.timetrackerfrontend;
 
-//import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.NumberPicker;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ManageCategoryActivity extends AppCompatActivity {
-
-    private final String projectName = "TestProject";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_category);
 
-        TextView textView = (TextView) findViewById(R.id.tVProjetname);
-        textView.setText(this.projectName);
+        final ListView listView = (ListView) findViewById(R.id.listCategories);
+        String [] values = {"Organisation", "Entwicklung", "Testdaten erzeugen"};
+
+        final ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < values.length; i++) {
+            list.add(values[i]);
+        }
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.content_listview_categories, R.id.categorieName, list);
+        listView.setAdapter(adapter);
 
         final FloatingActionButton btnFloatingAdd = (FloatingActionButton) findViewById(R.id.btnFlotingAdd);
         btnFloatingAdd.setOnClickListener(new View.OnClickListener() {
